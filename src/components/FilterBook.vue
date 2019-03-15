361717f9949ed6749cd319da923e2436c971aff9
jonathanslima <jonathan633.jl@gmail.com>
2019-03-15 06:11:43 -0300

    init filter

---
+++ src/components/FilterBook.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="filter">
+		<div class="row">
+			<div class="col s12">
+				<div class="input-field col s12">
+		          <input id="last_name" type="text" class="validate">
+		          <label for="last_name">Filter</label>
+		        </div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style scoped lang="scss">
+	
+</style>
+
